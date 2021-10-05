db59987a9213f1e06c946c26132ac2e8d713ebce
Nutifafa Afi Attor <attorfafa@gmail.com>
2021-10-05 15:31:45 +0000

    Update

--- firstProject/main.py
+++ firstProject/main.py
@@ -1,1 +1,22 @@
 print('Hello. This is my first program in Python.')
+# The "#" symbol is used for single line commenting in Python
+"""
+This ("""  """)is for
+multiline comments
+"""
+#Creating variables
+storeVariable = "Nutifafa"
+numberVariable = 10
+compoundVariable = 10+4
+
+print(storeVariable)
+print(numberVariable)
+print(compoundVariable)
+
+#arrays/list
+b = ["Afi", "Attor"]
+print(b)
+print(b[1])
+
+
+
